b6875a529d4f10cd4b8e73a527e541d5a7689daa
loukassideris <gokuthelegend@gmail.com>
2023-01-03 15:10:19 +0200

    Initial commit

---
+++ functions.py
@@ -0,0 +1,16 @@
+FILEPATH = "todos.txt"
+
+
+def get_todos(filepath=FILEPATH):
+    """ Read a text file and return the list of
+    to-do items.
+    """
+    with open(filepath, "r") as todo_file:
+        func_todos = todo_file.readlines()
+    return func_todos
+
+
+def write_todos(todos_arg, filepath=FILEPATH):
+    """ Write the to-do items list in the text file. """
+    with open(filepath, "w") as filepath:
+        filepath.writelines(todos_arg)
