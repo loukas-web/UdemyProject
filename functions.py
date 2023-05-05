ab61fa5e73e2ada84e7047bf07073f8de6e9f5ab
loukassideris <gokuthelegend@gmail.com>
2023-05-05 22:48:38 +0300

    GUI

--- functions.py
+++ functions.py
@@ -14,3 +14,8 @@ def write_todos(todos_arg, filepath=FILEPATH):
     """ Write the to-do items list in the text file. """
     with open(filepath, "w") as filepath:
         filepath.writelines(todos_arg)
+
+
+if __name__ == "__main__":
+    print("Hello")
+    print(get_todos())
