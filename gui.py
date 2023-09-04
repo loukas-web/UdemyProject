31d796f253adb3d8b4c0e0aee3322fe22a6070a2
loukas <loukas.sideris@protonmail.com>
2023-09-04 21:48:34 +0300

    adasdasd

--- gui.py
+++ gui.py
@@ -6,6 +6,7 @@ input_box = sg.InputText(tooltip="Enter todo")
 add_button = sg.Button("Add")
 
 window = sg.Window('My To-Do App', layout=[[label, input_box, add_button]])
+
 window.read()
-print("Hello")
+print('Hello')
 window.close()
